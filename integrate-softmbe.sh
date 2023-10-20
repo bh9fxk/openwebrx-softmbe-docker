7cc3992464e6075065f2ee7f59e19e0da92bdc01
Charles Cui <alavin.cui@gmail.com>
2023-10-20 21:20:56 +0800

    Update integrate-softmbe.sh

--- integrate-softmbe.sh
+++ integrate-softmbe.sh
@@ -53,7 +53,7 @@ _EOF_
 
 apt-get -qq update
 apt-get -qq -y purge --autoremove --allow-remove-essential $BUILD_PACKAGES
-apt-get -qq -y purge --autoremove --allow-remove-essential wget gpg git debhelper cmake debconf  apt-utils
+apt-get -qq -y purge --autoremove --allow-remove-essential wget gpg git debhelper cmake debconf
 apt-get -y autoremove
 apt-get autoclean
 
